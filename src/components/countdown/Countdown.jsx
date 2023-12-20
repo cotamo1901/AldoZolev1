a8b15af7a27ff176395f95c744fcb8592fcf5e6b
SofiDevO <angiemeow89@gmail.com>
2023-12-20 11:24:22 -0600

    Se agregan estilos

--- src/components/countdown/Countdown.jsx
+++ src/components/countdown/Countdown.jsx
@@ -1,19 +1,22 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 const Countdown = () => {
   const calculateTimeLeft = () => {
     const currentDate = new Date();
-    const targetDate = new Date('December 25, 2023 00:00:00');
+    const targetDate = new Date("December 25, 2023 00:00:00");
     const difference = targetDate - currentDate;
 
     if (difference > 0) {
       const days = Math.floor(difference / (1000 * 60 * 60 * 24));
-      const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const hours = Math.floor(
+        (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
       const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((difference % (1000 * 60)) / 1000);
 
-      return { days, hours, minutes };
+      return { days, hours, minutes, seconds };
     } else {
-      return { days: 0, hours: 0, minutes: 0 };
+      return { days: 0, hours: 0, minutes: 0, seconds: 0 };
     }
   };
 
@@ -29,26 +32,60 @@ const Countdown = () => {
 
   return (
     <div className="flex justify-center items-center h-screen">
-      <div className="w-full max-w-xl px-4 md:max-w-full"> 
+      <div className="w-full max-w-xl px-4 md:max-w-full">
         <div className="bg-gray-200 border rounded p-8 w-full">
-          <h2 className="text-2xl font-bold mb-4">La cuenta regresiva ha comenzado en:</h2>
-          <div className="flex justify-end mb-4">
-            <p className="text-sm text-gray-500">Conéctate a este viaje sonoro</p>
+          <h2 className="text-2xl font-bold mb-4 text-[#613625]">
+            La cuenta regresiva ha comenzado en:
+          </h2>
+          <div className="flex justify-start mb-4 font-bold">
+            <p className="text-sm text-[#ba5f4d] text-left">
+              Conéctate a este viaje sonoro
+            </p>
           </div>
-          <div className="flex items-center justify-center">
-            <div className="bg-gray-200 p-4 rounded-lg">
-              <div className="text-4xl font-bold">{timeLeft.days}</div>
-              <div className="text-xs uppercase text-gray-600">días</div>
+          <div className="flex  justify-center gap-1 items-center ">
+            <div className="bg-gray-200 rounded-lg">
+              <div className="text-5xl font-bold text-center text-[#613625] px-5 py-5 bg-gray-300 rounded-lg">
+                {timeLeft.days}
+              </div>
+              <div className="text-xs uppercase text-[#613625] font-bold">
+                días
+              </div>
             </div>
+            <span className="text-4xl uppercase text-[#613625] font-bold pl-4 pb-5">
+              :
+            </span>
             <div className="mx-2"></div>
-            <div className="bg-gray-200 p-4 rounded-lg">
-              <div className="text-4xl font-bold">{timeLeft.hours}</div>
-              <div className="text-xs uppercase text-gray-600">horas</div>
+            <div className=" p-4 ">
+              <div className="text-5xl font-bold text-center text-[#613625]  px-5 py-5 bg-gray-300 rounded-lg">
+                {timeLeft.hours}
+              </div>
+              <div className="text-xs uppercase text-[#613625]  font-bold">
+                horas
+              </div>
             </div>
+            <span className="text-4xl uppercase text-[#613625] font-bold pl-4 pb-5">
+              :
+            </span>
             <div className="mx-2"></div>
-            <div className="bg-gray-200 p-4 rounded-lg">
-              <div className="text-4xl font-bold">{timeLeft.minutes}</div>
-              <div className="text-xs uppercase text-gray-600">minutos</div>
+            <div className="bg-gray-200 rounded-lg">
+              <div className="text-5xl font-bold text-center text-[#613625] px-5 py-5 bg-gray-300 rounded-lg">
+                {timeLeft.minutes}
+              </div>
+              <div className="text-xs uppercase text-[#613625] font-bold">
+                minutos
+              </div>
+            </div>
+            <span className="text-4xl uppercase text-[#613625] font-bold pl-4 pb-5">
+              :
+            </span>
+            <div className="mx-2"></div>
+            <div className="bg-gray-200  rounded-lg">
+              <div className="text-5xl font-bold text-center text-[#613625]  px-5 py-5 bg-gray-300 rounded-lg">
+                {timeLeft.seconds}
+              </div>
+              <div className="text-xs uppercase text-[#613625] font-bold">
+                segundos
+              </div>
             </div>
           </div>
           <div className="flex justify-start mt-4">
--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -7,19 +7,19 @@ export const Hero = () => {
   return (
     <section className="hero-container">
       <div>
-        <img src="../../../public/img/tao.png" alt="img" className="custom-img" />
-        <h3 className="custom-heading">
-          desde el 25 de diciembre
-        </h3>
+        <img
+          src="../../../public/img/tao.png"
+          alt="img"
+          className="custom-img"
+        />
+        <h3 className="custom-heading">Desde el 25 de diciembre</h3>
       </div>
 
       <div className="countdown-container">
         <div>
           <Countdown />
         </div>
-        <div>
-          {/* Otro contenido */}
-        </div>
+        <div>{/* Otro contenido */}</div>
       </div>
     </section>
   );
